5c934f58aecb29483450f6023d77605202d612d9
Maffolandro <ale.chiaro@hotmail.it>
2019-02-07 10:10:04 +0100

    Java - Decorator e Singleton

---
+++ Java/Singleton.java
@@ -0,0 +1,15 @@
+public class Singleton() {
+	// Istanza di riferimento
+	private static Singleton instance = null;
+	
+	// Funzione per ottenere l'istanza
+	public static synchronized get_instance() {
+		if (instance == null) {
+			instance = new Singleton();
+		}
+		return instance;
+	}
+	
+	// Costruttore privato per impedire l'istanziazione diretta
+	private Singleton() {};
+}
