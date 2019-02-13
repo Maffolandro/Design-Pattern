250cff8a88f4a04eb937e598b2ecf1f4c14fc867
Maffolandro <ale.chiaro@hotmail.it>
2019-02-13 12:24:42 +0100

    Create Singleton.py

---
+++ Singleton/Python/Singleton.py
@@ -0,0 +1,8 @@
+class Singleton():
+	_instance = Singleton(); # Riferimento all'unica istanza della classe
+	
+	# Crea un nuovo oggetto solamente se non esiste ancora, restituisce sempre il valore di __instance
+	def __new__(cls, *args, **kwargs):
+		if not cls._instance:
+			cls._instance = object.__new__(cls, *args, **kwargs);
+		return cls._instance;
