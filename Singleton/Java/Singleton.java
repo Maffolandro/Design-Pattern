b716fa987350fd1ee1d7aef6442c04dbdad5671b
Maffolandro <ale.chiaro@hotmail.it>
2019-02-13 10:00:57 +0100

    Update Singleton.java

--- Singleton/Java/Singleton.java
+++ Singleton/Java/Singleton.java
@@ -1,6 +1,9 @@
 package DesignPattern;
 
 public class Singleton {
+	/* #### Metodi per la definizione del Singleton ####
+	 * #### NON MODIFICARE ####
+	 */
 	private static Singleton instance = null; // Istanza della classe
 	
 	// Fornire "costruttore alternativo" che genera l'istanza se non presente, poi la richiama
@@ -12,12 +15,20 @@ public class Singleton {
 		return instance;
 	};
 	
-	// Sovrascrivere se il costruttore alternativo deve accettare parametri
+	// Prevenire chiamate esterne dal costruttore standard
+	private Singleton() {};
+
+	/* #### Fine metodi per la definizione del Singleton ####
+	 * #### MODIFICABILE DA QUI IN POI ####
+	 */
+	
+	// Definire initialize se il costruttore alternativo deve accettare parametri
 	private static void initialize() {
 		//
 		//
 	};
 	
-	// Prevenire chiamate esterne dal costruttore standard
-	private Singleton() {};
+	// #### Inserire qui variabili e logica per il Singleton ####
+	//
+	//
 }
