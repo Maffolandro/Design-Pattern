2c2828fd67134afa3f7c9843d381630e1892be47
Maffolandro <ale.chiaro@hotmail.it>
2019-02-13 09:58:46 +0100

    Update SingletonExample.java
    Modified since subclassing is not allowed.

--- Singleton/Java/SingletonExample.java
+++ Singleton/Java/SingletonExample.java
@@ -2,14 +2,34 @@
  * Call getVal to get the value.
  */
 
-from DesignPatterns import Singleton
-
-public class Counter extends Singleton {
-	private static int val;
+public class SingletonCounter {
+	/* #### Metodi per la definizione del Singleton ####
+	 * #### NON MODIFICARE ####
+	 */
+	private static Singleton instance = null; // Istanza della classe
 	
-	private static void initialize(int amount) {
+	// Fornire "costruttore alternativo" che genera l'istanza se non presente, poi la richiama
+	public static synchronized getInstance(Object... args) {
+		if (instance == null) {
+			instance = new Singleton();
+		}
+		initialize(args);
+		return instance;
+	};
+	
+	// Prevenire chiamate esterne dal costruttore standard
+	private SingletonCounter() {};
+
+	/* #### Fine metodi per la definizione del Singleton ####
+	 */
+	
+	// Definire initialize se il costruttore alternativo deve accettare parametri
+	private static void initialize() {
 		val = amount;
-	}
+	};
+	
+	// #### Inserire qui variabili e logica per il Singleton ####
+	private static int val;
 	
 	private static void initialize() {
 		val = 0;
@@ -28,10 +48,10 @@ public class Counter extends Singleton {
 
 public class SingletonExample {
 	public static void main(String[] args) {
-		Counter.getInstance(10); // set to 10
-		Counter.addval(10); // add 10
-		Counter.getInstance(); // set to 0
-		Counter.addval(15);  // add 15
-		System.out.println("Valore di Counter: " + Counter.getval()); // Should be 15
+		SingletonCounter.getInstance(10); // set to 10
+		SingletonCounter.addval(10); // add 10
+		SingletonCounter.getInstance(); // set to 0
+		SingletonCounter.addval(15);  // add 15
+		System.out.println("Valore di Counter: " + SingletonCounter.getval()); // Should be 15
 	}
 }
