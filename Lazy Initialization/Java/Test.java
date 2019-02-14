581fdaecc8a4f33a94226e879d0d60539942963e
Maffolandro <ale.chiaro@hotmail.it>
2019-02-14 15:38:35 +0100

    Create Test.java

---
+++ Lazy Initialization/Java/Test.java
@@ -0,0 +1,21 @@
+import org.junit.*
+
+public class Test {
+  @Test
+  void checkBeforeCall() {
+  
+  }
+  
+  @Test
+  void checkAfterCall() {
+  
+  }
+  
+  @Test
+  void checkTiming() {
+  
+  }
+}
+
+Controllare stato prima e dopo l'inizializzazione;
+ Controllare tempi di esecuzione alla prima e alle successive chiamate;
