e3d010cc37a98d21be7276f97e236bb920f7c16e
Maffolandro <ale.chiaro@hotmail.it>
2019-02-14 17:18:06 +0100

    Create EXAMPLE.java

---
+++ Example/Python/EXAMPLE.java
@@ -0,0 +1,13 @@
+class Lazy:
+  #### NON MODIFICARE ####
+  ##
+  ##
+  #### MODIFICARE DA QUI IN POI ####
+  
+  #### DATI PER EFFETTUARE I TEST ####
+  ##
+  ##
+
+#### CODICE PER VEDERE IL FUNZIONAMENTO ####
+##
+##
