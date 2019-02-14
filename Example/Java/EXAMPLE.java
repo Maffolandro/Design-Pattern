c2470caee22266a90f7dc7767ee0a207aaaeee3a
Maffolandro <ale.chiaro@hotmail.it>
2019-02-14 17:00:50 +0100

    Create EXAMPLE.java

---
+++ Example/Java/EXAMPLE.java
@@ -0,0 +1,19 @@
+public class EXAMPLE {
+  
+  //// NON MODIFICARE ////
+  //
+  //
+  //
+  //// MODIFICARE DA QUI IN POI ////
+  
+  
+  
+  //// DATI PER EFFETTUARE I TEST ////
+  //
+  //
+}
+
+  public static void main(String[] args) {
+    
+  }
+}
